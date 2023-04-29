e66af6aa8ff277d542491df2cdf062ce43dcca6d
Andrey <teryaev.2004@mail.ru>
2023-04-29 15:47:58 +0300

    restart-project. Delete-react. finish-layout-desktop

--- src/components/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-const Loader: FC = () => {
-  return (
-    <div className="lds-dual-ring"></div>
-  )
-};
-
-export default Loader;
--- src/store/searchStore.ts
+++
@@ -1,7 +0,0 @@
-import { createStore, createEvent } from "effector";
-
-export const setSearchValue = createEvent<string>();
-
-export const $searchStore = createStore<string>("");
-
-$searchStore.on(setSearchValue, (state, data) => (state = data));
